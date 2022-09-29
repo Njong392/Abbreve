151112cfe065b3ec462721001c9aef615264a65b
Njong <njongemy@gmail.com>
2022-09-29 23:28:52 +0100

    build: added functionality to search bar

--- abbreve-project/src/components/form.jsx
+++ abbreve-project/src/components/form.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
 const Form = () => {
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(false);
+    const [userInput, setUserInput] = useState();
+
+    function handleChange(event) {
+        setUserInput(event.target.value.toLowerCase());
+    }
+
+    const fetchData = (event) => {
+        event.preventDefault();
+
+        fetch('https://njong392.github.io/data/server.json')
+        .then(response => {
+            if(!response.ok){
+                throw Error('Resource not found. 404.');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setData(data);
+            setError(false);
+        })
+        .catch(err => {
+            console.log(err.message);
+            setError(true);
+        });
+    }
+  
     return(
         <div className=" bg-dark py-12 px-14">
             <section className="block justify-center pb-16 md:flex">
@@ -7,17 +37,30 @@ const Form = () => {
                     <p className="text-gray text-sm mt-5">*For now, abbreviations are one-way. For example, Idk can only translate to 'I don't know', and not the other way round.</p>     
                 </div>
 
-                <div className="bg-ash m-3 h-11 rounded-full flex items-center p-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6 text-purple">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                    </svg>
+                <div>
+                   <form className="block md:flex items-center gap-3">
+                        <div className="bg-ash h-11 rounded-full flex items-center p-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6 text-purple">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                            </svg>
 
-                    <input type="search" placeholder="Search slang full meaning..." className="flex-1 w-1/2 h-11 rounded-full ml-2 border-none outline-none text-gray text-lg" />
+                            <input type="search" placeholder="Search slang full meaning..." className="flex-1 w-1/2 h-11 rounded-full ml-2 border-none outline-none text-gray text-lg" onChange={handleChange} />
+
+                        </div>
+
+                        <button className='bg-purple text-ash text-sm font-bold rounded-xl hover:scale-110 outline-none mt-2' onClick={fetchData}>Submit</button>
+                   </form>
+
+                    {error && <div className="text-purple text-sm mt-2">Oopsie. Some error occured :(</div>}
+                    {data && <div className="mt-2 text-purple font-bold text-xl ml-2"><p>{data[`${userInput}`]['definition']}</p></div>}
+                    
+
                 </div>
+              
             </section>
 
         </div>
     )
 }
 
-export default Form
+export default Form;
