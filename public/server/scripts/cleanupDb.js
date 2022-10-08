990000ed4f346f53153e4de51afe0a47b0702361
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-10-08 10:28:31 +0000

    feat: added cli

---
+++ public/server/scripts/cleanupDb.js
@@ -0,0 +1,70 @@
+import { mkdir, rm, writeFile } from "fs";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import chalk from "chalk";
+import { log } from "console";
+import { setAbbrFilesMap } from "../lib/dbManager.js";
+
+const directory = resolve(dirname(fileURLToPath(import.meta.url)), "../db");
+
+export default function cleanupDb() {
+  return new Promise((resolve, reject) => {
+    rm(
+      directory,
+      {
+        recursive: true,
+        force: true,
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        // resolve("dskkjfsfkj");
+        // return;
+
+        mkdir(
+          directory,
+          {
+            recursive: true,
+          },
+          (err) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            writeFile(
+              `${directory}/.gitignore`,
+              "",
+              {
+                flag: "w",
+              },
+              (err) => {
+                if (err) {
+                  reject(err);
+                  return;
+                }
+
+                resolve("DB chunks were cleared successfully");
+              }
+            );
+
+            setAbbrFilesMap({});
+          }
+        );
+      }
+    );
+  });
+}
+
+cleanupDb()
+  .then((message) => {
+    log(chalk.bgGreen("CLEAN UP JOB SUCCESSFUL!"), "\n====\n");
+    log(chalk.green(message));
+  })
+  .catch((err) => {
+    log(chalk.bgRed("CLEAN UP JOB FAILED!"), "\n====\n");
+    log(chalk.red(err));
+  });
