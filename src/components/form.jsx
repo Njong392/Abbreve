d950f9752ec69d658f8b0477b5f80a11968e1729
FaZil-shaik <fazilshaik103@gmail.com>
2024-04-18 09:41:01 +0530

    #426 optimized code for Clear button

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,96 +1,137 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { LoadingSpinner } from "./loadingSpinner";
 import copyImage from "../assets/copy.png";
+
 const searchParams = new URLSearchParams(window.location.search);
 const prefillVar = searchParams.get("share");
 
 const Form = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(false);
-  const [userInput, setUserInput] = useState("");
-  const [isUserInputBlank, setIsUserInputBlank] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const previousUserInput = useRef(undefined);
-  const hasUserInputChanged = previousUserInput.current !== userInput;
-  const [isCopied, setIsCopied] = useState(false);
+  const [formState, setFormState] = useState({
+    data: null,
+    error: false,
+    errorMessage: false,
+    userInput: "",
+    isUserInputBlank: false,
+    isLoading: false,
+    isCopied: false,
+  });
 
-  function clearDataBeforeFetch() {
-    setErrorMessage("");
-    setData(false);
-    setIsUserInputBlank(false);
-    setError(false);
-  }
+  const previousUserInput = useRef(undefined);
+  const hasUserInputChanged = previousUserInput.current !== formState.userInput;
+
+  const clearDataBeforeFetch = () => {
+    setFormState((prevState) => ({
+      ...prevState,
+      errorMessage: "",
+      data: null,
+      isUserInputBlank: false,
+      error: false,
+    }));
+  };
 
   const fetchData = (e) => {
     if (hasUserInputChanged) {
       clearDataBeforeFetch();
     }
 
-    setIsLoading(true);
+    setFormState((prevState) => ({ ...prevState, isLoading: true }));
     e.preventDefault();
-    const url = `/server/db/${userInput}.json`;
+    const url = `/server/db/${formState.userInput}.json`;
 
-    if (userInput.trim().length === 0) {
-      setIsUserInputBlank(true);
-      setIsLoading(false);
+    if (formState.userInput.trim().length === 0) {
+      setFormState((prevState) => ({
+        ...prevState,
+        isUserInputBlank: true,
+        isLoading: false,
+      }));
     } else {
       fetch(`${url}`)
         .then((response) => {
           if (response.status === 404) {
-            setIsUserInputBlank(false);
-            setErrorMessage(true);
-            setIsLoading(false);
+            setFormState((prevState) => ({
+              ...prevState,
+              isUserInputBlank: false,
+              errorMessage: true,
+              isLoading: false,
+            }));
           } else if (!response.ok) {
-            setIsLoading(false);
+            setFormState((prevState) => ({
+              ...prevState,
+              isLoading: false,
+            }));
             throw Error("Resource not found");
           }
           return response.json();
         })
         .then((data) => {
-          setData(data);
-          setError(false);
-          setErrorMessage(false);
-          setIsUserInputBlank(false);
-          setIsLoading(false);
+          setFormState((prevState) => ({
+            ...prevState,
+            data: data,
+            error: false,
+            errorMessage: false,
+            isUserInputBlank: false,
+            isLoading: false,
+          }));
         })
         .catch((err) => {
           console.log(err.message);
-          setErrorMessage(true);
-          setIsUserInputBlank(false);
-          setIsLoading(false);
+          setFormState((prevState) => ({
+            ...prevState,
+            errorMessage: true,
+            isUserInputBlank: false,
+            isLoading: false,
+          }));
         });
     }
   };
 
   const copyToClipboard = (e) => {
     e.preventDefault();
-    const link = `${window.location.origin}/?share=${userInput}`;
+    const link = `${window.location.origin}/?share=${formState.userInput}`;
     navigator.clipboard
       .writeText(link)
       .then(() => {
         console.log("Link copied to clipboard");
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 2000);
+        setFormState((prevState) => ({ ...prevState, isCopied: true }));
+        setTimeout(
+          () =>
+            setFormState((prevState) => ({ ...prevState, isCopied: false })),
+          2000
+        );
       })
       .catch((err) => {
         console.error("Could not copy text: ", err);
       });
   };
 
+  const deleteAbbreviation = () => {
+    setFormState({
+      data: null,
+      error: false,
+      errorMessage: false,
+      userInput: "",
+      isUserInputBlank: false,
+      isLoading: false,
+      isCopied: false,
+    });
+  };
+
   useEffect(() => {
-    if (!isUserInputBlank) {
-      previousUserInput.current = userInput;
+    if (!formState.isUserInputBlank) {
+      previousUserInput.current = formState.userInput;
     }
-  }, [userInput]);
+  }, [formState.userInput]);
 
   useEffect(() => {
     if (prefillVar) {
-      setUserInput(prefillVar);
+      setFormState((prevState) => ({
+        ...prevState,
+        userInput: prefillVar,
+      }));
       const mockEvent = { preventDefault: () => {} };
       fetchData(mockEvent);
     }
-  }, [isUserInputBlank]);
+  }, [formState.isUserInputBlank]);
 
   return (
     <div className="py-5 md:mb-0 lg:py-12 px-[14px] dark:bg-dark">
@@ -131,31 +172,42 @@ const Form = () => {
                 type="text"
                 placeholder="Search slang full meaning..."
                 className="flex-1 w-[14rem] h-6 ml-2 border-none outline-none placeholder:text-black bg-ash"
-                value={userInput}
+                value={formState.userInput}
                 onChange={(e) => {
                   clearDataBeforeFetch();
-                  setUserInput(e.target.value.toLocaleLowerCase());
+                  setFormState((prevState) => ({
+                    ...prevState,
+                    userInput: e.target.value.toLocaleLowerCase(),
+                  }));
                 }}
               />
             </div>
 
             <button
               onClick={fetchData}
-              disabled={isLoading || !hasUserInputChanged}
+              disabled={formState.isLoading || !hasUserInputChanged}
               className="bg-deeppurple text-ash font-bold rounded-xl hover:scale-110 p-2 mt-4 md:mt-0 items-center flex justify-center h-[50px] min-w-[100px] w-full transition-all cursor-pointer md:w-auto">
-              {isLoading ? <LoadingSpinner /> : "Search"}
+              {formState.isLoading ? <LoadingSpinner /> : "Search"}
             </button>
+
+            {formState.userInput && (
+              <button
+                onClick={deleteAbbreviation}
+                className="bg-red-500 text-white font-bold rounded-xl hover:scale-110 p-2 mt-4 md:mt-0 items-center flex justify-center h-[50px] min-w-[100px] w-full transition-all cursor-pointer md:w-auto">
+                Delete
+              </button>
+            )}
           </form>
 
-          {data && (
+          {formState.data && (
             <div className="bg-ash shadow-lg border-2 border-deeppurple py-2 px-2 rounded-lg mt-4">
               <div className="mt-1 font-bold text-xl ml-2 text-deeppurple">
                 <p role="region" aria-live="assertive">
-                  {data.definition}
+                  {formState.data.definition}
                   <button
                     onClick={(e) => copyToClipboard(e)}
                     className="group relative inline-block float-right w-12 h-7 p-0 border-none focus:outline-none transition-transform duration-100 ease-out">
-                    {isCopied ? (
+                    {formState.isCopied ? (
                       <span className="absolute inset-0 flex items-center justify-center font-bold text-sm px-1 -ml-5 h-full">
                         Copied
                       </span>
@@ -170,18 +222,18 @@ const Form = () => {
                 </p>
               </div>
               <div className="mt-2 text-gray font-bold text-md ml-2 dark:text-gray">
-                <p>{data.alternatives}</p>
+                <p>{formState.data.alternatives}</p>
               </div>
             </div>
           )}
 
-          {error && (
+          {formState.error && (
             <div className="text-deeppurple dark:text-purple text-sm mt-2 ">
-              Oops. Some connection error occured.
+              Oops. Some connection error occurred.
             </div>
           )}
 
-          {isUserInputBlank && (
+          {formState.isUserInputBlank && (
             <div className="mt-4">
               <p className="text-deeppurple dark:text-purple ">
                 Search bar 🔍 is Empty! Please input a slang.
@@ -189,7 +241,7 @@ const Form = () => {
             </div>
           )}
 
-          {errorMessage && (
+          {formState.errorMessage && (
             <div className="mt-4 p-3 lg:absolute">
               <p className="text-deeppurple dark:text-purple">
                 This entry does not exist in our records as of yet :(
