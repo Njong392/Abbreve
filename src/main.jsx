4d27f85bcca163e8e1391bba688c2c99a22a05dc
Njong <njongemy@gmail.com>
2025-03-07 16:14:12 +0100

    build: removed analytivs

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 // eslint-disable-next-line no-unused-vars
-import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
