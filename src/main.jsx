c2b9832c2406ecdf460988956c0f692cf3ce1c8a
Njong <njongemy@gmail.com>
2025-03-07 16:04:11 +0100

    build: added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+// eslint-disable-next-line no-unused-vars
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
